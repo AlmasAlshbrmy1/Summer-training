c74ec9c314825dd4ade29f80a9b8fd64e036f250
AlmasAlshbrmy1 <almas2442@hotmail.com>
2026-08-11 14:42:17 +0300

    Add files via upload

---
+++ Project/Phase2/registers.sv
@@ -0,0 +1,16 @@
+module registers #(
+    parameter WIDTH = 4
+)(
+    input  logic clk,
+    input  logic rstn,
+    input  logic en,
+    input  logic [WIDTH-1:0] data_in,
+    output logic [WIDTH-1:0] data_out
+);
+always_ff @(posedge clk or negedge rstn) begin
+    if (!rstn)
+        data_out <= '0;
+    else if (en)
+        data_out <= data_in;
+end
+endmodule
