c233b1bd7ea53f6500dc96337ede5afd589d8e0c
AlmasAlshbrmy1 <almas2442@hotmail.com>
2026-08-11 14:33:55 +0300

    Add files via upload

---
+++ Project/Phase2/Alu.sv
@@ -0,0 +1,16 @@
+
+module Alu #(parameter WIDTH=4)(
+    input logic [WIDTH-1:0] A,          // from Ra
+    input logic [WIDTH-1:0] B,          // from Rb
+    input logic     S,         
+    output logic [WIDTH-1:0] F,   
+    output logic      carry 
+);
+ 
+    logic [3:0] b_final;
+ 
+    assign b_final = B ^ {4{S}};
+ 
+    assign {carry, F} = A + b_final + S;
+ 
+endmodule
